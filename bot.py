f27f90a000bc509f1ff53c40e23cc294125e328d
293thd-spec <293thd@gmail.com>
2026-04-20 13:56:11 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,20 @@
 import os
 import requests
+import time
 
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 def send_telegram(msg):
+    if not TOKEN or not CHAT_ID:
+        print("Missing TOKEN or CHAT_ID")
+        return
+
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
 
 send_telegram("🚀 BOT STARTED")
+
+while True:
+    print("Bot running...")
+    time.sleep(60)
